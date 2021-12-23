295f014db20db96a2edf42e576ca04dc1cd34020
Maria Negri <mariamnegri@gmail.com>
2021-12-23 19:18:12 -0300

    heroku push

--- api/data/db-config.js
+++ api/data/db-config.js
@@ -1,6 +1,7 @@
 const knex = require('knex')
 const configs = require('../../knexfile')
 
+
 // Determine which environment is going to be run in Heroku
 // and determine whicn file environment will knex query
 module.exports = knex(configs[process.env.NODE_ENV])
