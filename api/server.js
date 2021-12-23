893daff50dd5bd642fbb1041b70213f10156ca24
Maria Negri <mariamnegri@gmail.com>
2021-12-23 12:27:41 -0300

    Auth router ready

--- api/server.js
+++ api/server.js
@@ -14,28 +14,6 @@ server.use(cors())
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
 
-// function getAllUsers() { return db('users') }
-
-// async function insertUser(user) {
-//   // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
-//   // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
-//   // UNLIKE SQLITE WHICH FORCES US DO DO A 2ND DB CALL
-//   const [newUserObject] = await db('users').insert(user, ['user_id', 'username', 'password'])
-//   return newUserObject // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
-// }
-
-
-
-// server.get('/api/users', async (req, res) => {
-//   res.json(await getAllUsers())
-// })
-
-// server.post('/api/users', async (req, res) => {
-//   res.status(201).json(await insertUser(req.body))
-// })
-
-
-//home page
 server.get('/', (req, res) => {
   res.status(200).json(
       {
